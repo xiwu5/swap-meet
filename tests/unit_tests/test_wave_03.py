96a4c9dc8ec0ffb89d2cbae42942ca5cc7ab152e
Joyce Wu <youngjoycewu@gmail.com>
2025-09-30 14:19:04 -0700

    done with Wave03!yay!

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -40,7 +40,7 @@ def test_swap_items_returns_true():
     assert item_b in jolie.inventory
     assert result
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_items_when_my_item_is_missing_returns_false():
     item_a = Item()
     item_b = Item()
@@ -67,7 +67,7 @@ def test_swap_items_when_my_item_is_missing_returns_false():
     assert item_e in jolie.inventory
     assert not result
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_items_when_their_item_is_missing_returns_false():
     item_a = Item()
     item_b = Item()
@@ -94,7 +94,7 @@ def test_swap_items_when_their_item_is_missing_returns_false():
     assert item_e in jolie.inventory
     assert not result
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_items_from_my_empty_returns_false():
     fatimah = Vendor(
         inventory=[]
@@ -114,7 +114,7 @@ def test_swap_items_from_my_empty_returns_false():
     assert len(jolie.inventory) == 2
     assert not result
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_items_from_their_empty_returns_false():
     item_a = Item()
     item_b = Item()
@@ -131,7 +131,5 @@ def test_swap_items_from_their_empty_returns_false():
 
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    raise Exception("Complete this test according to comments below.")
-    # *********************************************************************
-    # ****** Complete Assert Portion of this test **********
-    # *********************************************************************
+    assert not result
+    assert result is False
