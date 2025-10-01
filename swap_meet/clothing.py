ae5380021ef97fdc46c7a50af97d5c80032f6283
Anaiah <amaddox.dev@gmail.com>
2025-10-01 15:14:41 -0700

    Modified line 6 in clothing.py from is to ==

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,7 +3,7 @@ from swap_meet.item import Item
 class Clothing(Item):
     def __init__(self, id=None, condition=0, fabric="Unknown"):
         super().__init__(id, condition)
-        self.fabric = fabric if fabric is "Unknown" else fabric
+        self.fabric = fabric if fabric == "Unknown" else fabric
 
     def get_category(self):
         return "Clothing"
