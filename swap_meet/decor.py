b51830e46f102b59bde4696475f3af7bb159b8c4
Joyce Wu <youngjoycewu@gmail.com>
2025-10-01 01:26:26 -0700

    created inheritance relationship between Class Item and Class Decor.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,13 +1,12 @@
-import uuid
-class Decor:
-    def __init__(self, id=None, width=None, length=None):
-        self.id = int(uuid.uuid4()) if id is None else id
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self, id=None, condition=0, width=None, length=None):
+        super().__init__(id, condition)
         self.width = 0 if width is None else width
         self.length = 0 if length is None else length
-        # self.condition = 0.0 if condition is None else condition
     
     def get_category(self):
         return 'Decor'
     
     def __str__(self):
-        return f"An object of type Decor with id {self.id}. "f"It takes up a {self.width} by {self.length} sized space."
+        return f"An object of type {self.get_category()} with id {self.id}. "f"It takes up a {self.width} by {self.length} sized space."
