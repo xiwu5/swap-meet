405a171f5397ad821fe224804f571db2a5aeab0f
Joyce Wu <youngjoycewu@gmail.com>
2025-10-01 01:23:18 -0700

    Created inhereritance relationship between Class Item and Clothing. Clothing is the child class of IItem.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,8 @@
-import uuid
+from swap_meet.item import Item
 
-class Clothing:
-    def __init__(self, id=None, fabric="Unknown"):
-        self.id = int(uuid.uuid4()) if id is None else id
+class Clothing(Item):
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
+        super().__init__(id, condition)
         self.fabric = fabric if fabric is "Unknown" else fabric
         # self.condition = 0.0 if condition is None else condition
 
@@ -10,4 +10,4 @@ class Clothing:
         return "Clothing"
     
     def __str__(self):
-        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        return f"An object of type {self.get_category()} with id {self.id}. It is made from {self.fabric} fabric."
