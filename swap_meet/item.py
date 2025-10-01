607946b72cf65dfc1f02a7948abb254eb495542e
Joyce Wu <youngjoycewu@gmail.com>
2025-10-01 01:10:25 -0700

    changed initialization of attribute "condition" as 0 since it's stated on README, "None" means this parameter is optional.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,9 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None, condition=None):
+    def __init__(self, id=None, condition=0):
         self.id = int(uuid.uuid4()) if id is None else id
-        self.condition = 0.0 if condition is None else condition
+        self.condition = condition
 
     def get_category(self):
         return "Item"
